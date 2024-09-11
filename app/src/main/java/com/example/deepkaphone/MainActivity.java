3905fb336b9b36b07c26707daa92e08afd359169
Deep Shekhar Singh <deepshekhar0306@gmail.com>
2024-09-11 22:23:11 +0530

    initial commit

---
+++ app/src/main/java/com/example/deepkaphone/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.deepkaphone;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                Toast.makeText(MainActivity.this, "Hi click listner worked !!", Toast.LENGTH_SHORT).show();
+//                String s = editText.getText().toString();
+//                int kg =  Integer.parseInt(s);
+//                double pound = kg * 2.205;
+//                textView.setText("The value in pound is : "+pound+" pound");
+//            }
+//        } );
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+    }
+    public void calculate( View view ) {
+        String s = editText.getText().toString();
+        int kg =  Integer.parseInt(s);
+        double pound = kg * 2.205;
+        textView.setText("The value in pound is : "+pound+" pound");
+    }
+
+}
